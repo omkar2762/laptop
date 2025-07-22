c9379a2d8c7c3fb99eca4a31545d57872fb24f3c
omkar2762 <omkarshinde2762@gmail.com>
2025-07-22 11:39:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+1) // Arithmatic operator 
+a=10
+b=20
+console.log(a+b);
+op=30
+
+2)// find reminder 
+console.log(17%3);
+op=2
+
+3)// multiply three number 
+console.log(2*3*4);
+op=24
+
+4)// subtract 100 from 250 and diuvide 5
+console.log (250-100) / 5);
+op=30
+
+5)// what will be the output
+let a=5;
+let b=++a;
+consol.log(a,b);
+ op=6
+
+
+6)//  use += operator add 10 varable ineteliza as s20
+let x =20;
+x + = 10;
+consol.log (x);
+OP=30
+
+7)// use *= double the value of varable inetelize as 7
+let y=7;
+y *=2;
+console.log(y);
+op=14
+
+
+8) // what will be the output
+let x=10;
+x -=3;
+console.log9(x);
+op=7
+
+9) // check if two number are equal using ==or==
+console.log(10 == '10');
+console.log(10=="10");
+
+
+    10) //greater number 
+console.log(25>15);
+
+   
+11) // compare two number and print true 
+a=10
+b=20
+console.log(a<b);
+op=true
+
+
+12)// what is the output 
+consol.log (5=="5")
+ op=equal
